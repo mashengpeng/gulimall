ad438cea10625d92efbdff68bff07abbd7d4bb3c
mashengpeng <1360311705@qq.com>
2022-04-13 02:33:21 +0800

    项目结构初始化

---
+++ gulimall-coupon/src/test/java/com/msp/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msp.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
